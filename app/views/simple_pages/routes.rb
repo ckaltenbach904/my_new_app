f10dd5dd0ef650d2ab43da15dd889ca62e010876
ckaltenbach904 <43072057+ckaltenbach904@users.noreply.github.com>
2019-04-17 17:03:59 +0200

    Delete routes.rb

--- app/views/simple_pages/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get 'simple_pages/about'
-  get 'simple_pages/contact'
-  get 'simple_pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#index'
-end
