b7ee13fe186efbc6dafb158fa429f2ce8e9c71dd
catharina kaltenbach <catharina904@outlook.com>
2019-04-21 22:08:17 +0200

    3.9

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,4 +1,12 @@
 class SimplePagesController < ApplicationController
   def index
   end
+
+  def contact
+  end
+
+  def thank_you
+    flash[:notice] = "Your query has been received!"
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#index'
+  post 'simple_pages/thank_you'
 end
