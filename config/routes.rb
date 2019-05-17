ab54548f5b3c57c98820f9c3a4b0cad0e7a24e50
catharina kaltenbach <catharina904@outlook.com>
2019-05-17 14:10:14 +0200

    5.1 Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   get 'simple_pages/products'
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/contact'
   get 'simple_pages/landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
   post 'simple_pages/thank_you'
 end
