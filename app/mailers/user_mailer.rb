671bf382ae0619d7af689d76cf1d1ce60a9e42f2
catharina kaltenbach <catharina904@outlook.com>
2019-06-04 14:23:17 +0200

    5.11

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
            to: "sfdabilene2006@gmail.com",
            subject: "A new contact form message from #{name}")
     end
+
+    def welcome(user)
+      @appname = "Bike Berlin"
+      mail(to: user.email,
+           subject: "Welcome to #{@appname}!")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
+  devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: { registrations: "user_registrations" }
   resources :users
   resources :products do
     resources :comments
