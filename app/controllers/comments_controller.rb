403401aa5c571cc4cb83f88e81da446b8356b76f
catharina kaltenbach <catharina904@outlook.com>
2019-06-18 17:35:08 +0200

    6.10 AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,8 +10,10 @@ class CommentsController < ApplicationController
 
         respond_to do |format|
             if @comment.save
+              @product = Product.find(params[:product_id])
               format.html { redirect_to @product, notice: 'Review was created successfully.' }
               format.json { render :show, status: :created, location: @product }
+              format.js
             else
               format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
               format.json { render json: @comment.errors, status: :unprocessable_entity }
