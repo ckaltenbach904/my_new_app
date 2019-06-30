9995ae3b9a56f912f012fbb6c50dc8c91b3261d2
catharina kaltenbach <catharina904@outlook.com>
2019-06-30 15:54:54 +0200

    6.13 Action Cable

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,7 +2,7 @@ module ProductsHelper
     def cache_key_for_products
         count          = @products.count
         max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
-        "products/all-#{count}-#{max_updated_at}"
+        "products/all-#{count}-#{max_updated_at}-#{params[:page]}"
     end
 
     def cache_key_for_product
