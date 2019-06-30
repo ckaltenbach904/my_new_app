2ccc01ab9a7a3f80527625566de38533a3dc8a15
catharina kaltenbach <catharina904@outlook.com>
2019-06-30 17:46:10 +0200

    6.14 Active Job

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,6 +1,7 @@
 App.product = App.cable.subscriptions.create("ProductChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
+    this.listen_to_comments();
   },
 
   disconnected: function() {
@@ -11,5 +12,17 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     // Called when there's incoming data on the websocket for this channel
     console.log("data received: ", data);
     $(".alert.alert-info").show();
+    $('.product-reviews').prepend(data.comment);
+    $("#average-rating").attr('data-score', data.average_rating);
+    refreshRating();
+  },
+  listen_to_comments: function() {
+    return this.perform('listen', {
+      product_id: $("[data-product-id]").data("product-id")
+    });
   }
+});
+
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,4 +9,5 @@ class Comment < ApplicationRecord
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 end
